3c63ec164d1244049076fadfdb6ac7e33dda2f0a
SidonTheTroll <siddhantborah431@gmail.com>
2025-01-09 12:40:22 +0530

    Fixed comment styling

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -5,5 +5,5 @@ return {
   { "preservim/tagbar", lazy = false },
   { "Pocco81/auto-save.nvim", lazy=false },
   { "mhinz/vim-startify", lazy=false}
---  { "andweeb/presence.nvim", lazy=false }
+-- { "andweeb/presence", lazy=false }
 }
