7e6e435ebb9854111532631fc6931f5911cf1177
SidonTheTroll <siddhantborah431@gmail.com>
2025-01-09 20:56:22 +0530

    Actually made configs for colemak

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,7 +15,20 @@ map("i", "jk", "<ESC>")
 --  \ \_____\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \_\ \ \_\ 
 --   \/_____/   \/_____/   \/_____/     \/_/   \/_____/   \/_/  \/_/ 
 
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-f>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-i>", "<Right>", { desc = "move right" })
+map("i", "<C-n>", "<Down>", { desc = "move down" })
+map("i", "<C-e>", "<Up>", { desc = "move up" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>i", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>n", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>e", { desc = "switch window up" })
+
 map("n", "<F8>","<cmd> TagbarToggle<cr>", {desc = "Toggle Tagbar on or off"})
 map("n", "<F5>", '<cmd> ASToggle<cr>', {desc = 'Toggle Autosave'})
+
 map("n", "<F6>", "<cmd> split<cr>", {desc = "Split editor horizontally"})
 map("n", "<F7>", "<cmd> vsplit<cr>", {desc = "Split editor vertically"})
