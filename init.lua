b3b4ca27468f472d7254b92d1fa4ceab93f6d251
SidonTheTroll <siddhantborah431@gmail.com>
2025-01-09 12:12:54 +0530

    Added actual line highlighting

--- init.lua
+++ init.lua
@@ -42,12 +42,19 @@ end)
 --  \ \_____\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \_\ \ \_\ 
 --   \/_____/   \/_____/   \/_____/     \/_/   \/_____/   \/_/  \/_/ 
 
--- Copy yanked text to System Clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- VimStartify Activation and ASCII file path
-local config_file_path = vim.fn.expand('$HOME/.config/nvim/ascii.txt')
-
-vim.g.startify_custom_header = vim.fn.filereadable(config_file_path) == 1
-  and vim.fn.readfile(config_file_path)
-  or { "Welcome to Neovim!" }
+-- -- Copy yanked text to System Clipboard
+-- vim.opt.clipboard = "unnamedplus"
+--
+-- -- VimStartify Activation and ASCII file path
+-- local config_file_path = vim.fn.expand('$HOME/.config/nvim/ascii.txt')
+--
+-- vim.g.startify_custom_header = vim.fn.filereadable(config_file_path) == 1
+--   and vim.fn.readfile(config_file_path)
+--   or { "Welcome to Neovim!" }
+--
+-- -- Activate Relative Line Number in every boot 
+-- vim.o.relativenumber = true
+--
+-- -- Activate Cursor Line and Column
+-- vim.o.cursorline = true
+-- vim.o.cursorcolumn = true
