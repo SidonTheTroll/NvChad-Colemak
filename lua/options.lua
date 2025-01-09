bf96e8c85dc88c9e9e5cbf04f4ca8dc6a6f39611
SidonTheTroll <siddhantborah431@gmail.com>
2025-01-09 12:29:54 +0530

    Fixed options file

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+require "nvchad.options"
+
+-- Copy yanked text to System Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- VimStartify Activation and ASCII file path
+local config_file_path = vim.fn.expand('$HOME/.config/nvim/ascii.txt')
+
+vim.g.startify_custom_header = vim.fn.filereadable(config_file_path) == 1
+  and vim.fn.readfile(config_file_path)
+  or { "Welcome to Neovim!" }
+
+-- Activate Relative Line Number in every boot 
+vim.o.relativenumber = true
+
+-- Activate Cursor Line and Column
+vim.o.cursorlineopt ='both'
+vim.o.cursorcolumn = true
