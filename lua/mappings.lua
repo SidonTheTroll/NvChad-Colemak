4557d95b9bf11304ece83d254b2dc56a8599a2e6
SidonTheTroll <sidonthetroll@gmail.com>
2025-07-18 23:07:01 +0530

    fine tuned settings for colemak-dh

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,23 +16,37 @@ map("i", "jk", "<ESC>")
 --   \/_____/   \/_____/   \/_____/     \/_/   \/_____/   \/_/  \/_/ 
 
 -- Navigation
-
-map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-d>", "<Home>", { desc = "move beginning of line" })
 map("i", "<C-f>", "<End>", { desc = "move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-m>", "<Left>", { desc = "move left" })
 map("i", "<C-i>", "<Right>", { desc = "move right" })
 map("i", "<C-n>", "<Down>", { desc = "move down" })
 map("i", "<C-e>", "<Up>", { desc = "move up" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>i", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>n", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>e", { desc = "switch window up" })
+map("n", "<C-w>m", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-w>i", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-w>n", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-w>e", "<C-w>k", { desc = "switch window up" })
+
+-- Change functions 
+map('i', "<C-h>", "<CR>", { desc = "Alternate for return key" })
+
+-- nvim-cmp
+local cmp = require("cmp")
+
+cmp.setup({
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<CR>']  = cmp.mapping.confirm({ select = true }),
+        ['<C-h>']  = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }
+})
 
 -- Toggles 
-
 map("n", "<F8>","<cmd> TagbarToggle<cr>", {desc = "Toggle Tagbar on or off"})
-map("n", "<F5>", '<cmd> ASToggle<cr>', {desc = 'Toggle Autosave'})
+map("n", "<F5>", '<cmd> ASToggle<cr>', {desc = "Toggle Autosave"})
 
 map("n", "<F6>", "<cmd> split<cr>", {desc = "Split editor horizontally"})
 map("n", "<F7>", "<cmd> vsplit<cr>", {desc = "Split editor vertically"})
